39d71de8915a4b84d1fdff84775fb81612132b3b
Alex <ataffe@asu.edu>
2016-08-31 22:05:33 -0700

    added plus button and title page timer

--- app/src/main/java/com/alex/homeworkplanner/Title.java
+++ app/src/main/java/com/alex/homeworkplanner/Title.java
@@ -1,14 +1,34 @@
 package com.alex.homeworkplanner;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Title extends AppCompatActivity {
 
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                titleToHome();
+            }
+        };
+
+        handler.postDelayed(runnable,7000);
     }
+
+    public void titleToHome(){
+        Intent intent = new Intent(this,MainPage.class);
+        startActivity(intent);
+    }
+
+
 
 }
